ba20d6031cc93cd64db82381cbada8b336311b64
tomdamo <20182376@student.hhs.nl>
2023-01-16 22:41:48 +0100

    Afrekenen begin, alleen redirect erna werkt niet.

--- theaterlaakreact/src/components/Afrekenen.js
+++ theaterlaakreact/src/components/Afrekenen.js
@@ -1,35 +1,49 @@
-import React from 'react'
-import {Form, Alert, Button, FormGroup, Input, Label} from 'reactstrap'
-
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { FormGroup } from 'reactstrap';
 export function Afrekenen() {
+  var details ={
+    amount: "100", //kosten <- moet met functionaliteit worden bepaalt van de winkelwagen. useState
+    reference: "19", //betaling id <-generate per betaling, per keer dat er op submit wordt geklikt(?)
+    url: 'https://localhost:3000/succes', //terugverwijzing aan het eind van de betaling
+  }
+  var options = {
+    headers: {"content-type": "application/x-www-form-urlencoded"}
+  }
+  var betaalsite = "https://fakepay.azurewebsites.net/";
+  
+
+  const [streamBody, setBody] = useState("")
+  useEffect(() => {
+    axios.post(betaalsite, details, options)
+    .then(response=>
+      {
+        console.log(response)
+        setBody(response.data)
+      return response.data
+      }).then((response) => {
+        if(response.data){
+          window.location.assign(response.data)
+        }
+  
+      })
+    .catch(err=>
+      console.log(err))
+  })
+  document.body.innerHTML = streamBody
   return (
       <>
     <h1>Afrekenen</h1>
         {/*Post request naar website met amount, reference(id van betaling),
          url(teruggewezen na betaling, naar /success of /cancel)
         website: https://fakepay.azurewebsites.net/ */}
-<Form>
-  <FormGroup>
-    <Label for="rekeningnummer">
-    Rekening nummer van koper
-    </Label>
-    <Input
-      id="rekeningnummer"
-      name="rekeningnummer"
-      placeholder="rekeningnummer"
-      type="text"
-    />
-  </FormGroup>
-  <Button onClick={gelukt}>
-    Submit
-  </Button>
-  </Form>
+        {streamBody}
+        
+
       </>
   )
+  
 }
+  
 
-function gelukt(){
-alert(
-  "Betaling gelukt! U wordt doorverwezen...")
-}
 export default Afrekenen
