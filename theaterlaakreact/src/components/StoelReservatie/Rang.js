8dd9adfddad0228c938a3a619c7136a264cc9d40
welkom <20154887@student.hhs.nl>
2023-01-17 06:08:52 +0100

    xx

--- theaterlaakreact/src/components/StoelReservatie/Rang.js
+++ theaterlaakreact/src/components/StoelReservatie/Rang.js
@@ -1,30 +1,42 @@
-import React, {useState} from "react";
-import { Button, ListGroup, ListGroupItem, ListGroupItemHeading} from "reactstrap";
+import React, { useState } from "react";
+import {
+  Button,
+  ListGroup,
+  ListGroupItem,
+  ListGroupItemHeading,
+} from "reactstrap";
 import Stoel from "./Stoel.Js";
 
 const Rang = (props) => {
-const [item, setItem] = useState(props.propOne);
-const [beschikbareStoelen, setBeschikbareStoelen] = useState(props.propOne.Stoelen)
+  const [item, setItem] = useState(props.propOne);
+  const [beschikbareStoelen, setBeschikbareStoelen] = useState(props.propOne.Stoelen);
+  const [gesStoelen, setGesStoelen] = useState([]);
 
-const handleClick= () => {
-    
-}
+  const handleClick = (e) => {
+    props.onClickGereseerveerd(current =>[...current,<Button color="primary"><div className="mx-2"></div>{e}</Button>])
+  };
 
-const stoelenLijst = beschikbareStoelen.map(s => <ListGroupItem key={s.StoelId} action><div className="m-3">Stoelnummer {s.StoelId}</div>  </ListGroupItem>)
-
-
+  const stoelenLijst = beschikbareStoelen.map((s) => (
+    <ListGroupItem
+      className=""
+      onClick={()=>handleClick(s.StoelId)}
+      key={s.StoelId}
+      action
+    >
+      <div className="m-3">Stoelnummer {s.StoelId}</div>{" "}
+    </ListGroupItem>
+  ));
 
   return (
-    <>  
-    <div className="row my-3">
-        <ListGroupItemHeading color="primary"> Rij {item.RangId}</ListGroupItemHeading>
-        <ListGroup>{stoelenLijst}</ListGroup>
+    <>
+      <div className="row my-3">
+        <div className="col">
+          <ListGroupItemHeading color="primary">
+            <p>Rij {item.RangId}</p>
+          </ListGroupItemHeading>
+          <ListGroup>{stoelenLijst}</ListGroup>
         </div>
-
-    
-    
-
-    
+      </div>
     </>
   );
 };
--- theaterlaakreact/src/components/StoelReservatie/StoelReservatie.js
+++ theaterlaakreact/src/components/StoelReservatie/StoelReservatie.js
@@ -2,24 +2,42 @@ import React, { useState, useEffect } from "react";
 import { Button } from "reactstrap";
 import data from "./data.json";
 import Rang from "./Rang";
-import "./StoelReservatie.css";
+import StoelSelectie from "./StoelSelectie";
 
 const StoelReservatie = () => {
   const [stoelen, setStoelen] = useState(data);
   const [gereserveerdestoelen, setGereserveerdeStoelen] = useState([]);
   const [mijnstoelen, mijnStoelen] = useState([]);
 
-  const rangen = stoelen.map((s) => <Rang propOne={s} />);
+  // useEffect(()=>{
+  //   gereserveerdestoelen.forEach(element => {
+  //     if(stoelen.some(element)){
+  //       setStoelen(current => current.filter(element))
+  //     }
+  //   });
+  // }, [stoelen]
+  // )
+
+  const rangen = stoelen.map((stoel) => (
+    <Rang stoelen={stoelen} onClicksetStoelen={setStoelen}onClickGereseerveerd={setGereserveerdeStoelen} propOne={stoel} />
+  ));
 
   return (
     <>
+  
       <div className="container">
         <div className="row justify-content-center ">
           <div className="col-6">{rangen}</div>
           <div className="col">
             <div className="row sticky-top mt-5">
-              <p>Mijn Gereserveerde stoelen:</p>
-              <Button color="success">Afronden</Button>
+              Mijn Gereserveerde stoelen:
+              <div className="row">
+                <div className="col d-inline-flex flex-wrap my-2 gap-2">
+                  {gereserveerdestoelen}
+                </div>
+                <Button color="success">Afronden</Button>
+              </div>
+              
             </div>
           </div>
         </div>
