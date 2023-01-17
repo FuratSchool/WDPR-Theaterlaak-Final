6801cd41548547b10d79d02a8d63b9a165de5142
welkom <20154887@student.hhs.nl>
2023-01-17 01:40:29 +0100

    commit

--- theaterlaakreact/src/components/StoelReservatie/StoelReservatie.js
+++ theaterlaakreact/src/components/StoelReservatie/StoelReservatie.js
@@ -2,18 +2,27 @@ import React, { useState, useEffect } from "react";
 import { Button } from "reactstrap";
 import data from './data.json';
 import AllRang from './AllRang';
-import GeselecteerdeStoelen from './GeselecteerdeStoelen';
+import GereserveerdeStoelen from './GereserveerdeStoelen';
 
 
 const StoelReservatie = () => {
+  const [stoelenLijst] = useState(data)
+  const [gereserveerdestoelenLijst, setGereserveerdeStoelenLijst] = useState([])
+  const [mijnstoelenLijst, mijnStoelenLijst] = useState([])
+
   return (
     <>
       <div className="container">
         <div className="row justify-content-center ">
-          <div className="col-6"> <AllRang parentToChild={data} /></div>
-          <div className="col"><GeselecteerdeStoelen /></div>
+          <div className="col-6"> <AllRang parentToChild={stoelenLijst} /></div>
+          <div className="col">
+            <p>Mijn Gereserveerde stoelen:</p>
+            <Button color="success">Afronden</Button>
+            </div>
         </div>
+        
       </div>
+      
     </>
   )
 }
