f21a9c7640e99b3e9021046070bf1a21263cb029
welkom <20154887@student.hhs.nl>
2023-01-21 13:58:41 +0100

    stoel 1* relatie

--- TheaterLaakAPi/Models/Stoel.cs
+++ TheaterLaakAPi/Models/Stoel.cs
@@ -7,7 +7,7 @@ public class Stoel
     public int isInvalide { get; set; }
 
 
-    public Reservering Reserveringen { get; set; }
+    public ICollection<Reservering> Reserveringen { get; set; }
 
     public int RangId { get; set; }
     public Rang Rang { get; set; }
