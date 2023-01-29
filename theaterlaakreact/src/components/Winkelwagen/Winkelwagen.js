b7772957f38091fdb820a238393d35ffb422177d
tomdamo <20182376@student.hhs.nl>
2023-01-29 21:20:19 +0100

    verder..

--- theaterlaakreact/src/components/Winkelwagen/Winkelwagen.js
+++ theaterlaakreact/src/components/Winkelwagen/Winkelwagen.js
@@ -10,6 +10,7 @@ export const Winkelwagen = (args, { cart }) => {
   const [user, setUser] = useState([]);
   const authHeader = useAuthHeader();
   const [winkelwagen, setWinkelwagen] = useState([]);
+  const [totPrijs, setTotPrijs] = useState(0);
 
   const jwtAuthenticationHeader = {
     headers: {
@@ -26,38 +27,47 @@ export const Winkelwagen = (args, { cart }) => {
         );
         console.log(response);
         setUser(response.data.id);
+        const fetchWinkelwagen = async () => {
+          try {
+            const response = await axios.get(
+              "http://localhost:5044/getReservering/" + user
+            );
+            console.log(response);
+            setWinkelwagen(response.data);
+          } catch (err) {
+            console.error(err);
+          }
+        };
+        fetchWinkelwagen();
       } catch (err) {
-        // Handle Error Here
-        console.error(err);
-      }
-    };
-    // var requestRoute = "http://localhost:5044/getReservering/" + user;
-    console.log(user);
-
-    const fetchWinkelwagen = async () => {
-      try {
-        const response = await axios.get(
-          "http://localhost:5044/getReservering/" + user
-        );
-        console.log(response);
-        setWinkelwagen(response.data);
-      } catch (err) {
-        // Handle Error Here
         console.error(err);
       }
     };
     FetchLoggedUser();
-    fetchWinkelwagen();
-  }, []);
+  }, [user]);
 
-  const [reservaties, setReservaties] = useState([]);
-  function handleAddreservatie(reservatie) {
-    setReservaties([...reservaties, reservatie]);
-  }
+  // const TotaalPrijs = winkelwagen.prijs.reduce(
+  //   (eersteprijs, currentprijs, index) => eersteprijs + currentprijs,
+  //   0
+  // );
+  // console.log(TotaalPrijs);
+
   return (
     <>
       <h1>Uw Winkelwagen</h1>
-      {JSON.stringify(winkelwagen)}
+      {/* {JSON.stringify(winkelwagen)} */}
+
+      {winkelwagen.map((item, index) => (
+        <div key={index}>
+          {"Voorstelling " +
+            item.voorstelling +
+            " Stoel nummer " +
+            item.stoelNr +
+            ", €" +
+            item.prijs}
+        </div>
+      ))}
+      {totPrijs}
     </>
   );
 };
