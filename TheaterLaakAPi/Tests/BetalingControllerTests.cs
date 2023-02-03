e98126a6c989135c65f7a06ba501a219666c4173
tomdamo <20182376@student.hhs.nl>
2023-02-03 12:20:30 +0100

    mock gebruikt voor betaling

--- TheaterLaakAPi/Tests/BetalingControllerTests.cs
+++ TheaterLaakAPi/Tests/BetalingControllerTests.cs
@@ -1,76 +1,81 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Moq;
 using TheaterLaakAPi.Controllers;
+using TheaterLaakAPi.Models;
+using Xunit;
 
-namespace Tests;
-
-public class BetalingControllerTests
+namespace Tests
 {
-    private readonly DatabaseContext _context;
-    private readonly BetalingController _controller;
-
-    [Fact]
-    public async Task test_betaling_true()
+    public class BetalingControllerTests
     {
-        // Arrange
-        var options = new DbContextOptionsBuilder<DatabaseContext>()
-            .UseInMemoryDatabase(databaseName: "Test_true_db")
-            .Options;
+        private readonly Mock<DatabaseContext> _mockContext;
+        private readonly BetalingController _controller;
 
-        using (var context = new DatabaseContext(options))
+        public BetalingControllerTests()
         {
-            var controller = new BetalingController(context);
+            _mockContext = new Mock<DatabaseContext>();
+            _controller = new BetalingController(_mockContext.Object);
+        }
 
+        [Fact]
+        public async Task TestSuccesBetaling()
+        {
+            // Arrange
             var betaling = new Betaling
             {
                 id = 1,
                 succes = true
             };
 
+            _mockContext.Setup(c => c.Betaling.Add(It.IsAny<Betaling>())) //voegt een instantie v Betaling toe aan mockContext. Any instantie van betaling kan meegegeven worden aan add
+                .Callback<Betaling>(b => b.id = betaling.id); // betaling Id aan de betaling property id toegevoegd.
+            _mockContext.Setup(c => c.SaveChangesAsync(default))  //default om de default waarde van de cancellationtoken te gebruiken
+                .ReturnsAsync(1); //1 om de aantal entities die zijn toegevoegd te simulaten, 1 entity toegevoegd.
+
             // Act
-            var result = await controller.PostBetaling(betaling);
+            var result = await _controller.PostBetaling(betaling);
 
             // Assert
             var redirectResult = Assert.IsType<RedirectResult>(result);
             Assert.Equal("http://localhost:3000/Succes", redirectResult.Url);
         }
-    }
-    [Fact]
-    public async Task test_betaling_false()
-    {
-        // Arrange
-        var options = new DbContextOptionsBuilder<DatabaseContext>()
-            .UseInMemoryDatabase(databaseName: "Test_false_db")
-            .Options;
 
-        using (var context = new DatabaseContext(options))
+        [Fact]
+        public async Task TestFalseBetaling()
         {
-            var controller = new BetalingController(context);
-
+            // Arrange
             var betaling = new Betaling
             {
                 id = 1,
                 succes = false
             };
 
+            _mockContext.Setup(c => c.Betaling.Add(It.IsAny<Betaling>()))
+                .Callback<Betaling>(b => b.id = betaling.id);
+            _mockContext.Setup(c => c.SaveChangesAsync(default))
+                .ReturnsAsync(1);
+
             // Act
-            var result = await controller.PostBetaling(betaling);
+            var result = await _controller.PostBetaling(betaling);
 
             // Assert
             var redirectResult = Assert.IsType<RedirectResult>(result);
             Assert.Equal("http://localhost:3000/Cancel", redirectResult.Url);
         }
+
+        [Fact]
+        public async Task testBetalingNotFound()
+        {
+            // Arrange
+            _mockContext.Setup(c => c.Betaling.FindAsync(1))
+                .ReturnsAsync((Betaling)null);
+
+            // Act
+            var result = await _controller.GetBetaling(1);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result.Result);
+        }
     }
-    [Fact]
-    public async Task test_betaling_notFound()
-    {
-        // Arrange
-
-        // Act
-        var result = await _controller.GetBetaling(1);
-
-        // Assert
-        Assert.IsType<NotFoundResult>(result.Result);
-    }
-
 }
